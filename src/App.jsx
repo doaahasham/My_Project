e39c039de578dc5182e955df539170e4cde1540d
doaahasham <doohesham452@gmail.com>
2026-06-26 15:25:05 +0300

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+
+import Navbar from "./header/navbar";
+import Footer from "./footer/footer";
+
+import Home from "./home/home";
+import AboutUs from "./about_us/aboutus";
+import ContactUs from "./contact_us/contactus";
+import Mens from "./mens/mens";
+import Kids from "./kids/kids";
+import Womens from "./womens/women";
+import Accessories from "./accessories/accessories";
+import LatestProduct from "./latestproduct/LatestProducts";
+import TopDeals from "./topdeals/topdeals";
+import LogIn from "./login/Login";
+import Kind from "./kindshopping/kind";
+import Registration from "./login/registration";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/men" element={<Mens />} />
+        <Route path="/kid" element={<Kids />} />
+        <Route path="/women" element={<Womens />} />
+        <Route path="/accessories" element={<Accessories />} />
+        <Route path="/latestproduct" element={<LatestProduct />} />
+        <Route path="/topdeals" element={<TopDeals />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/kind" element={<Kind />} />
+      </Routes>
+
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
